61e53be8a7129876090b5a0b44db63925829982d
Gbemisola Kotoye <barkty@yahoo.com>
2022-05-22 19:56:38 +0100

    landing page updates

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,10 +8,23 @@ const Landing = () => {
                 <h1 className='landing-title'>SOMETHING AWESOME IS COMING SOON</h1>
                 <p className='landing-text'>Your all-in-one affliate marketing tracking software <b>track, automate and optimize </b>your campaigns</p>
                 <div className='landing-time'>
-                    <div className=''>
-
+                    <div className='landing-hour'>
+                        <h2>7</h2>
+                        <p>Days</p>
                     </div>
-                </div>
+                    <div className='landing-hour'>
+                        <h2>24</h2>
+                        <p>Hours</p>
+                    </div>
+                    <div className='landing-hour'>
+                        <h2>54</h2>
+                        <p>Minutes</p>
+                    </div>
+                    <div className='landing-hour'>
+                        <h2>11</h2>
+                        <p>Seconds</p>
+                    </div>
+                </div>1
             </div>
         </div>
     )
