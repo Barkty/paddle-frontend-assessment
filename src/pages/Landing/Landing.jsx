c11f3f399a450f9798b098a4dcbd89e76d571260
Gbemisola Kotoye <barkty@yahoo.com>
2022-05-22 21:10:29 +0100

    landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,6 +4,9 @@ const Landing = () => {
 
     return (
         <div className='landing'>
+            <div className='bubble'></div>
+            <div className='bubble2'></div>
+            <div className='bubble3'></div>
             <div className='landing-wrap'>
                 <h1 className='landing-title'>SOMETHING AWESOME IS COMING SOON</h1>
                 <p className='landing-text'>Your all-in-one affliate marketing tracking software <b>track, automate and optimize </b>your campaigns</p>
@@ -24,7 +27,25 @@ const Landing = () => {
                         <h2>11</h2>
                         <p>Seconds</p>
                     </div>
-                </div>1
+                </div>
+                <form className='landing-form'>
+                    <div className='form-group'>
+                        <div className='form-group-div'>
+                            <input type='text' name='firstname' placeholder='First Name.' className='form-group-input'/>
+                        </div>
+                        <div className='form-group-div'>
+                            <input type='text' name='lastname' placeholder='Last Name.' className='form-group-input'/>
+                        </div>
+                    </div>
+                    <div className='form-group-cta'>
+                        <div className='form-group-email'>
+                            <input type='email' name='email' placeholder='Enter your email address.' className='form-group-input-email'/>
+                        </div>
+                        <button type='submit' className='form-group-btn'>
+                            Join our waiting list
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     )
